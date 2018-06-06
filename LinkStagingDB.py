e001550bb566a13749ff578b923cc3c50cdbbb15
bopozam <bopo@zam.com>
2018-06-06 11:55:24 -0700

    formatting fixes

--- LinkStagingDB.py
+++ LinkStagingDB.py
@@ -11,13 +11,13 @@ import socket
 
 Today = datetime.datetime.now().strftime('%y-%m-%d')
 Weekly = datetime.timedelta(weeks=1)
+db_identifier = 'wowhead-mysql-staging-%s' %Today
+rds = boto3.client('rds', region_name='us-east-1')
+route53 = boto3.client('route53', region_name='us-east-1')
+zone_id = '/hostedzone/Z174UMT6MD8IR8'
+#boto3.set_stream_logger('botocore')
 
 def main():
-    db_identifier = 'wowhead-mysql-staging-%s' %Today
-    rds = boto3.client('rds', region_name='us-east-1')
-	route53 = boto3.client('route53', region_name='us-east-1')
-	zone_id = '/hostedzone/Z174UMT6MD8IR8'
-	#boto3.set_stream_logger('botocore')
 	
     running = True
     while running:
