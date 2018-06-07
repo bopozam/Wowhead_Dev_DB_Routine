c9e7aa62122c210f3b8dcceb6d29638a8a66ff3f
bopozam <bopo@zam.com>
2018-06-06 23:57:27 -0700

    ,.

--- DeleteDevDBRR.py
+++ DeleteDevDBRR.py
@@ -6,16 +6,22 @@ import datetime
 import fnmatch
 import os
 import re
+import socket
 
-Today = datetime.datetime.now().strftime("%y-%m-%d")
-TimeDelta = datetime.timedelta(days=5)
+Today = datetime.datetime.now().strftime('%y-%m-%d')
+Weekly = datetime.timedelta(weeks=1)
+db_identifier = 'wowhead-mysql-staging-%s' %Today
+rds = boto3.client('rds', region_name='us-east-1')
+route53 = boto3.client('route53', region_name='us-east-1')
+zone_id = '/hostedzone/Z174UMT6MD8IR8'
+#boto3.set_stream_logger('botocore')
 BeforeToday = datetime.datetime.now() - datetime.timedelta(days=1)
 
-Yesterday = Today - 60*60*24
+Yesterday = datetime.datetime.now() - 60*60*24
 if fileCreation < twodays_ago:
     print "File is more than two days old"
 
-def main():
+def delete():
     db_identifier = 'wowhead-mysql-staging-%s' %BeforeToday.strftime("%y-%m-%d")
     rds = boto3.client('rds', region_name='us-east-1')
     instances = rds.get_all_dbinstances.all()
@@ -23,8 +29,6 @@ def main():
     try:
 	for instance in rds.get_all_dbinstances():
 		print instance.id	#Prints all existing instances#
-		if instance.id =< db_identifier
-			print db_identifier
 
 	if db_identifier < Yesterday;
 		print db_identifier "is older"
@@ -38,31 +42,6 @@ def main():
         else:
             raise
 
-
-#    running = True
-#    while running:
-#        response = rds.describe_db_instances(DBInstanceIdentifier=db_identifier)
-#
-#        db_instances = response['DBInstances']
-#        if len(db_instances) != 1:
-#            raise Exception('Whoa cowboy! More than one DB instance returned; this should never happen')
-#
-#        db_instance = db_instances[0]
-#
-#        status = db_instance['DBInstanceStatus']
-#
-#        print 'Last DB status: %s' % status
-#
-#        time.sleep(5)
-#        if status == 'available':
-#            endpoint = db_instance['Endpoint']
-#            host = endpoint['Address']
-#            # port = endpoint['Port']
-#
-#            print 'DB instance ready with host: %s' % host
-#            running = False
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == '__delete__':
+    delete()
 
