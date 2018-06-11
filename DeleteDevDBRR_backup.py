9d3e98b0d4aaa018e115182bc6c8ef4feb4705f2
bopozam <bopo@zam.com>
2018-06-11 14:51:38 -0700

    new rewrite again

---
+++ DeleteDevDBRR_backup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import time
+import boto3
+import botocore
+import datetime
+import fnmatch
+import os
+import re
+import socket
+import sys
+
+Today = datetime.datetime.now().strftime('%y-%m-%d')
+Weekly = datetime.timedelta(weeks=1)
+db_identifier = 'wowhead-mysql-staging-%s' %Today
+rds = boto3.client('rds', region_name='us-east-1')
+route53 = boto3.client('route53', region_name='us-east-1')
+zone_id = '/hostedzone/Z174UMT6MD8IR8'
+#boto3.set_stream_logger('botocore')
+
+
+from boto.rds import connect_to_region
+from datetime import datetime, timedelta
+
+try:
+	days = int(sys.argv[1])
+except IndexError:
+	days = 7
+
+delete_time = datetime.utcnow() - timedelta(days=days)
+
+filters = {
+	'tag-key': 'mybackups'
+}
+
+print 'Deleting any instances older than {days} days'.format(days=days)
+
+instances = rds.describe_db_instances(filters=filters)
+
+deletion_counter = 0
+size_counter = 0
+
+for instance in instances:
+	start_time = datetime.strptime(
+		instance.start_time,
+		'%Y-%m-%dT%H:%M:%S.000Z'
+	)
+
+	if start_time < delete_time:
+		print 'Deleting {id}'.format(id=instance.id)
+		deletion_counter = deletion_counter + 1
+		size_counter = size_counter + instance.volume_size
+		# Just to make sure you're reading!
+		instance.delete(dry_run=True)
+
+print 'Deleted {number} instances totalling {size} GB'.format(
+	number=deletion_counter,
+	size=size_counter
+)
