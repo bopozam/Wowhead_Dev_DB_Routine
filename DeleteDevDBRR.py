9962e96a62b32263c5ff1a549b069638ce4c3c46
bopozam <bopo@zam.com>
2018-06-11 14:04:17 -0700

    slight changeds

--- DeleteDevDBRR.py
+++ DeleteDevDBRR.py
@@ -23,7 +23,6 @@ def all_rds_instances(region, page_size=20):
 
     page_size [explain what this does] should be bound between 20 and 100.
     """
-    client = boto3.client('rds', region_name=region)
     marker = ""
     pool = []
     while True:
@@ -32,56 +31,10 @@ def all_rds_instances(region, page_size=20):
 
         if marker is None:
             break
-        result = client.describe_db_instances(MaxRecords=page_size, Marker=marker)
+        result = rds.describe_db_instances(MaxRecords=page_size, Marker=marker)
         marker = result.get("Marker")
         pool = result.get("DBInstances")
 
-for instance in all_rds_instances({"region": "eu-west-1"}):
+for instance in all_rds_instances({"region": "us-east-1"}):
     print instance["Endpoint"]["Address"]
 
-# def delete():
-#     rds = boto3.client('rds', region_name='us-east-1')
-# 	
-#     
-#     try:
-#     	old_instance = rds.describe_db_instances(DBInstanceIdentifier='wowhead-mysql-staging-'.)
-#     	db_instances = old_instance['DBInstances']
-#     	db_instance = db_instances[0]
-#     	
-# 		InstanceCreateTime = db_instance['InstanceCreateTime']
-# 		
-#         print 'Creating RDS Read Replica instance with ID: %s' % db_identifier
-#     except botocore.exceptions.ClientError as e:
-#         if 'DBInstanceAlreadyExists' in e.message:
-#             print 'DB instance %s exists already, continuing to poll ...' % db_identifier
-#         else:
-#             raise
-# 
-# 
-#     running = True
-#     while running:
-#         response = rds.describe_db_instances(DBInstanceIdentifier=db_identifier)
-# 
-#         db_instances = response['DBInstances']
-#         if len(db_instances) != 1:
-#             raise Exception('More than one DB instance returned; this should never happen')
-# 
-#         db_instance = db_instances[0]
-# 
-#         status = db_instance['DBInstanceStatus']
-# 
-#         print 'Last DB status: %s' % status
-# 
-#         time.sleep(5)
-#         if status == 'available':
-#             endpoint = db_instance['Endpoint']
-#             host = endpoint['Address']
-#             # port = endpoint['Port']
-# 
-# #             print 'DB instance ready with host: %s' % host
-#             running = False
-# 
-# 
-# if __name__ == '__delete__':
-#     delete()
-# 
