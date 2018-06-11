e2647665120681846fed89cb7bb4c4ee141657fc
bopozam <bopo@zam.com>
2018-06-11 14:34:49 -0700

    complete rewrite

--- DeleteDevDBRR.py
+++ DeleteDevDBRR.py
@@ -16,25 +16,14 @@ route53 = boto3.client('route53', region_name='us-east-1')
 zone_id = '/hostedzone/Z174UMT6MD8IR8'
 #boto3.set_stream_logger('botocore')
 
-def all_rds_instances(region, page_size=20):
-    """
-    Gets all the RDS instances in a generator (lazy iterator) so you can implement it as:
-    `for instance in all_rds_instances(region):`
-
-    page_size [explain what this does] should be bound between 20 and 100.
-    """
-    marker = ""
-    pool = []
-    while True:
-        for instance in pool:
-            yield instance
-
-        if marker is None:
-            break
-        result = rds.describe_db_instances(Filters=[{"Name": "DBInstances:DBInstanceIdentifier","Values": ["wowhead-mysql-staging*"]},], MaxRecords=100, Marker=marker)
-        marker = result.get("Marker")
-        pool = result.get("DBInstances")
-
-for instance in all_rds_instances({"region": "us-east-1"}):
-    print instance["Endpoint"]["Address"]
-
+try:
+# get all of the db instances
+    dbs = rds.describe_db_instances()
+    for db in dbs['DBInstances']:
+        print ("%s %s") % (
+            db['DBInstanceIdentifier'],
+#             db['Endpoint']['Address'],
+#             db['Endpoint']['Port'],
+             db['DBInstanceStatus'])
+except Exception as error:
+	print error
