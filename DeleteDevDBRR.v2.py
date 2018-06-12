1e6f6b5feef62d983f9b390c605153b046ac39ea
bpeyatt <bpeyatt@BoPoMBPro.local>
2018-06-12 11:20:14 -0700

    added indents

--- DeleteDevDBRR.v2.py
+++ DeleteDevDBRR.v2.py
@@ -34,8 +34,8 @@ try:
 	db_instance = db_instances[0]
 	create_time = db_instance['InstanceCreateTime']
 
-for db in dbs['DBInstances']:
-	print 'Instance: %s' % db_instance 'was created on: %s' % create_time
+	for db in dbs['DBInstances']:
+		print 'Instance: %s' % db_instance 'was created on: %s' % create_time
 
 
 for ct in dbs['DBInstances']:
